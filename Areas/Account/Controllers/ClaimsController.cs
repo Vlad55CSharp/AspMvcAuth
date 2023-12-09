ffa5560d30119285700b895cfa080325fee7a822
Владислав Баженов <v.v.bazhenov@yandex.ru>
2023-12-10 04:31:09 +0600

    Claims

---
+++ Areas/Account/Controllers/ClaimsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using AspMvcAuth.Models;
+using System.Security.Claims;
+namespace AspMvcAuth.Areas.Account.Controllers
+{
+	[Authorize]
+	public class ClaimsController : Controller
+	{
+		[Route("{area}/{controller}")]
+		public IActionResult Index()
+		{
+			return View(User?.Claims);
+		}
+
+		[Route("{area}/{controller}/{action}")]
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[Route("{area}/{controller}/{action}")]
+		[HttpPost]
+		public async Task<IActionResult> Create([FromServices]UserManager<ApplicationUser> users, string claimType, string claimValue)
+		{
+			var user = await users.GetUserAsync(HttpContext.User);
+			Claim claim = new(claimType, claimValue, ClaimValueTypes.String);
+			IdentityResult result = await users.AddClaimAsync(user, claim);
+
+			if (result.Succeeded)
+				return LocalRedirect("/account/claims");
+			else
+				return BadRequest(result);
+		}
+
+		[HttpPost]
+		[Route("{area}/{controller}/{action}")]
+		public async Task<IActionResult> Delete([FromServices] UserManager<ApplicationUser> users, string claimValues)
+		{
+			var user = await users.GetUserAsync(HttpContext.User);
+
+			string[] claimValuesArray = claimValues.Split(";");
+			string claimType = claimValuesArray[0];
+			string claimValue = claimValuesArray[1]; 
+			string claimIssuer = claimValuesArray[2];
+
+			Claim claim = User.Claims.Where(x => x.Type == claimType && x.Value == claimValue && x.Issuer == claimIssuer).FirstOrDefault();
+
+			IdentityResult result = await users.RemoveClaimAsync(user, claim);
+
+			if (result.Succeeded)
+				return LocalRedirect("/account/claims");
+			else
+				return BadRequest(result);
+		}
+	}
+}
